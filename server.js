cc3ba23043a98452146d47103c66c2f8d5e788a8
nkartchner <39930903+nkartchner@users.noreply.github.com>
2019-01-15 22:23:22 -0800

    still broken

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ const app = express();
 
 const mongoose = require('mongoose');
 
+const path = require('path');
+
 const bodyParser = require('body-parser');
 
-app.use(express.static( __dirname + './dist/HelloAngluar' ));
+app.use(express.static( __dirname + './public/dist/public' ));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
+
+app.all("*", (request,response,next) => {
+    response.sendFile(path.resolve("./public/dist/public/index.html"));
+  });
 
 const routes = require('./server/config/routes');
 routes(app);
